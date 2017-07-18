4de55afe87590045c030aa1519de8cf575e281d3
Britt Thaels <user@Britts-iMac.local>
2017-07-18 22:15:40 +0200

    fixed bullet train theme for zsh

--- scripts/configuration-zsh.sh
+++ scripts/configuration-zsh.sh
@@ -5,3 +5,7 @@ git clone --recursive git@github.com:jeroenmols/zgen-configuration.git ~/.zgen
 ln -s ~/.zgen/.zshrc ~/.zshrc
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 cp files/Droid\ Sans\ Mono\ for\ Powerline.otf ~/Library/Fonts/Droid\ Sans\ Mono\ for\ Powerline.otf
+
+# Necessary for bullettrain theme to work: https://github.com/caiogondim/bullet-train.zsh/issues/212
+brew install nvm
+brew install node
