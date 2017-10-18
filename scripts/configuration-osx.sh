4c5f3cf8378ffff0edd30769e7320573a0870d56
Jeroen Mols <jmols.mobile@gmail.com>
2017-10-18 14:49:29 +0200

    Enabled faster cursor

--- scripts/configuration-osx.sh
+++ scripts/configuration-osx.sh
@@ -7,8 +7,8 @@ echo 'Customizing OS X configuration'
 #killall Dock
 
 # fast key repeat rate, requires reboot to take effect
-#defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
-#defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
+defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
+defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
 
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
